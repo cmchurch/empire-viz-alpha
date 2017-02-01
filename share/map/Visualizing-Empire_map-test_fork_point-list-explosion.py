01aaa97517e4041a41bd68c61459bd836f84ed2b
cmchurch <cmchurch@debian>
2017-02-01 11:44:29 -0800

    added fork of map-test and KWIC aggregator

---
+++ share/map/Visualizing-Empire_map-test_fork_point-list-explosion.py
@@ -0,0 +1,135 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#CHRISTOPHER M. CHURCH
+#ASSISTANT PROFESSOR OF HISTORY
+#UNIVERSITY OF NEVADA, RENO
+
+'''This script will generate a hexbin heatmap based on the locations identified from the NER.
+
+This is a fork of the original map test that explodes out the points, so that hexbin counts them all, rather than using weighting.
+Weighting was messing up the results due to some normalization algorithm I couldn't find documentation for.
+
+This isn't elegant, but it works.'''
+
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from matplotlib import cm as cm
+from matplotlib.colors import ListedColormap
+import os
+import unicodecsv
+
+
+#convert to meters for hexbin
+#def convert_lon_lat_points_to_meters_using_transform(points, tran):
+    # maybe there is a better way to get long/lat into meters but this works ok
+#    return np.array([tran(lon,lat) for lon,lat,size in points])
+
+#convert to meters for hexbin
+def convert_lon_lat_points_to_meters_using_transform(points, tran):
+    # maybe there is a better way to get long/lat into meters but this works ok
+    l = []
+    for lon,lat in points:
+        transform = tran(lon,lat)
+        l.append([transform[0],transform[1]])
+    return np.array(l)
+
+#make a custom colormap
+def make_colormap(seq):
+    """Return a LinearSegmentedColormap
+    seq: a sequence of floats and RGB-tuples. The floats should be increasing
+    and in the interval (0,1).
+    """
+    seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
+    cdict = {'red': [], 'green': [], 'blue': []}
+    for i, item in enumerate(seq):
+        if isinstance(item, float):
+            r1, g1, b1 = seq[i - 1]
+            r2, g2, b2 = seq[i + 1]
+            cdict['red'].append([item, r1, r2])
+            cdict['green'].append([item, g1, g2])
+            cdict['blue'].append([item, b1, b2])
+    return mcolors.LinearSegmentedColormap('CustomMap', cdict)
+
+
+#set working directory
+wd = "I:/Dropbox/NDAD/Visualizing-Empire/OpeNER/test"
+#wd = "F:/Dropbox/NDAD/Visualizing-Empire/OpeNER/test"
+os.chdir(wd)             
+
+#set matplotlib render settings
+#%matplotlib notebook
+#%matplotlib qt
+#%matplotlib inline
+
+# miller projection
+m = Basemap(projection='mill',lon_0=0)
+
+#make figure
+fig = plt.figure(figsize=(12,5))
+
+
+#draw label meridians and parallels.
+#m.drawparallels(np.arange(-90,90,30),labels=[1,0,0,0])
+#m.drawmeridians(np.arange(map.lonmin,map.lonmax+30,60),labels=[0,0,0,1])
+
+# fill continents 'lightgray' (with zorder=0), color wet areas 'lightblue'
+#m.drawmapboundary(fill_color='lightblue')
+#m.fillcontinents(color='lightgray',lake_color='lightblue')
+
+# plot coastlines
+m.drawcoastlines(linewidth="0.1")
+
+#get latlons
+size=[]
+lat=[]
+lon=[]
+filename="1880-p1-test.csv.geocoded.csv"
+with open(filename,'r') as f:
+    cfile = unicodecsv.reader(f,delimiter=",")
+    next(cfile)  # skip the headers
+    for row in cfile:
+        for i in range(0,int(row[1])):
+            lat.append(float(row[2]))
+            lon.append(float(row[3]))
+
+
+# In[ ]:
+
+#testing with weights
+#size=[127,106,98,78,71]
+#lat=[48.85340815,37.09024,47.20490744,45.99999986,19.99999754]
+#lon=[2.348798511,-95.712891,-1.548499151,1.999998463,76.99999562]
+
+
+#translate lat lon into map coordinates (meters) based on map projection
+
+points=zip(lon,lat)
+points = convert_lon_lat_points_to_meters_using_transform(points, m.projtran)
+
+#add points to map
+#m.plot(x, y, 'ko', markersize=1)
+#m.plot(points[:,0],points[:,1], 'ko', markersize=1)
+
+#add alpha transparency to color ramp
+# Choose colormap
+import matplotlib.colors as mcolors
+c = mcolors.ColorConverter().to_rgb
+#adjusted color map for legibility
+cmap = make_colormap([c('#FFFFFF'),c('#A83E3E'),0.5,c('#A83E3E'),c('#690303')])
+#straight linear colormap
+#cmap = make_colormap([c('white'),c('darkred')])
+
+# make plot using hexbin
+bins = 40
+#CS = m.hexbin(x=points[:,0],y=points[:,1],C=points[:,2],gridsize=bins,cmap=cmap) #C=points[:,2] is messing it up, so changed solution
+CS = m.hexbin(x=points[:,0],y=points[:,1],gridsize=bins,cmap=cmap) #without weighting -- weighting comes from list explosion
+m.colorbar(location="bottom",label="mentions in bin") # draw colorbar
+
+plt.title('Locations Mentioned in the Journal Day Voyages: 1880-p1')
+plt.gcf().set_size_inches(8,5)
+plt.show()
+
